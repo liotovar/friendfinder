a181422fb1a1c2bdb59af1331d437bb069dda0eb
liotovar <tovar.lionel@gmail.com>
2017-11-27 21:51:46 -0600

    Files added

---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,33 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friends = require("../data/friends.js");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    // handle incoming posts, handle compatibility logic
+    
+//    console.log(req.body);
+    profiles.push(req.body);
+    console.log('-----');
+    console.log(friends);
+});
+};
---
+++ server.js
@@ -0,0 +1,41 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080; //telling heroku what port we will be using
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false })); // body parser is a middleware package
+app.use(bodyParser.json()); // it packages it up nicely to send it to read nicely
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+//function is getting passed and also passing the variable app
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
